e7a02f3d58dd1c6a3ebf1196707959035ea9e0b6
skyisboss <skyisboss@gmail.com>
2022-06-01 13:15:47 +0800

    first commit

---
+++ src/log/index.ts
@@ -0,0 +1,32 @@
+import winston from 'winston'
+
+export const initLog = () => logger
+
+export const logger = winston.createLogger({
+  level: 'info',
+  defaultMeta: { service: 'telegraf-template' },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.simple(),
+    winston.format.errors({ stack: true }),
+    winston.format.prettyPrint()
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: './logs/error.log',
+      level: 'error'
+    }),
+    new winston.transports.File({ filename: './logs/combined.log' })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: './logs/exceptions.log' })
+  ]
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  )
+}
